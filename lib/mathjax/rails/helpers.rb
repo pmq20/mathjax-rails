abbbdbc0494b74948fbcec056497d94b85c26e0f
Daniel Puglisi <pulleasy@gmail.com>
2013-12-12 00:28:29 +0100

    fix how the config parameters is implemented into the mathjax_path

--- lib/mathjax/rails/helpers.rb
+++ lib/mathjax/rails/helpers.rb
@@ -2,13 +2,9 @@ module Mathjax
   module Rails
     module ViewHelpers
       def mathjax_tag(opt={})
-        if false==opt[:config]
-          additional = ''
-        else
-          opt[:config] = 'TeX-AMS_HTML-full.js' if !opt[:config]
-          additional = "?config=#{opt[:config]}"
-        end
-      	"<script src=\"#{mathjax_path(:uri=>'MathJax.js')}#{additional}\" type=\"text/javascript\"></script>".html_safe
+        opt[:config] ||= 'TeX-AMS_HTML-full.js'
+        opt[:config] = nil if opt[:config] == false
+      	"<script src=\"#{mathjax_path(:uri=>'MathJax.js', config: opt[:config])}\" type=\"text/javascript\"></script>".html_safe
       end
     end
   end
